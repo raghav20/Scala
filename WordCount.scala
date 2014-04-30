357496b2afd0c400d7d8fa2770b60a87e7e761d1
raghav20 <prash.raaghav@gmail.com>
2014-04-30 04:46:04 -0400

    Create WordCount.scala

---
+++ WordCount.scala
@@ -0,0 +1,11 @@
+ import scala.io.Source
+object Test {
+	def main(args: Array[String]){
+		val lines = Source.fromFile("D://Modi.txt").getLines.toArray
+		val emptyCounts = Map[String,Int]().withDefaultValue(0)
+		val words = lines.flatMap(line => line.split(" "))
+		val counts = words.foldLeft(emptyCounts)({(currentCounts: Map[String,Int], word: String) => currentCounts.updated(word, currentCounts(word) + 1)})
+		print(counts)
+       
+	}
+}
